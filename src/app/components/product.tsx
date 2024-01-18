c7657833e75e26e4b19be2d377303ecafcc17935
DieumegardGregory <dieumegard.gregory@gmail.com>
2024-01-18 17:28:07 +0100

    [ADD] random pictures

--- src/app/components/product.tsx
+++ src/app/components/product.tsx
@@ -1,5 +1,6 @@
 import { Grid } from "@mui/material";
-import { Image } from "@mui/icons-material";
+import Image from "next/image";
+import { picturesList } from "../utils/pictures";
 import styles from "../page.module.css";
 
 interface CardProductProps {
@@ -7,11 +8,22 @@ interface CardProductProps {
   prix: string;
 }
 
+
+
 export default function CardProduct(props: CardProductProps) {
   const { titre, prix } = props;
+
+  const definePicture = () => {
+    const randomPictureIndex = Math.floor(Math.random() * picturesList.length);
+    const picture = picturesList[randomPictureIndex];
+    return picture.split(".")[0];
+  }
+  
+  const randomPicture = definePicture();
+
   return (
     <Grid className={styles.vignette_product}>
-      <Image fontSize="large"></Image>
+      <Image src={`/${randomPicture}.jpeg`} width={100} height={100} alt={`${randomPicture}`} />
       <h3>{titre}</h3>
       <p>{prix}</p>
     </Grid>
---
+++ src/app/utils/pictures.ts
@@ -0,0 +1,10 @@
+export const picturesList = [
+    "hyperball",
+    "honorball",
+    "soinball",
+    "pokeball",
+    "rapideball",
+    "chronoball",
+    "appatball",
+    "memoireball",
+]
